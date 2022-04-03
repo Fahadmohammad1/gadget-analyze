492676cf6b4f08b1f6a5c87af407ad782392c2aa
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-04 03:37:32 +0600

    added some changes in banner

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,16 +4,19 @@ const Home = () => {
   return (
     <div className="md:flex justify-center items-center mt-8 ">
       <div className="flex flex-col justify-center">
-        <h1 className="text-4xl font-bold">
-          WELCOME TO PRO-NALYZE.
-          <span>CHECK YOUR FAV PRODUCT HERE.</span>
+        <h1 className="text-7xl font-extrabold font-sans">
+          Your next phone
+          <br />
+          <span className="text-[#4d3b69]">Your best phone</span>
         </h1>
-        <p>
+        <p className="text-justify my-4 pl-10">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum
           tenetur molestias quo consequuntur dolore laborum nihil, esse itaque
           odio fuga!
         </p>
-        <button>Live Demo</button>
+        <button className="bg-teal-200 p-3 rounded-xl font-bold mx-auto text-[#c94ced] ">
+          Live Demo
+        </button>
       </div>
       <div className="flex items-center">
         <img
