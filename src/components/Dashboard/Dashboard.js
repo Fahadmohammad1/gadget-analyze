3769645edd2fa05ceefd44bfd7cded7a0c1c2aaa
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-05 02:57:20 +0600

    dashboard title changed

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -51,33 +51,45 @@ const Dashboard = () => {
     },
   ];
   return (
-    <div className="md:flex mt-7">
-      <BarChart className="mx-auto" width={530} height={250} data={chartData}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="month" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="investment" fill="#8884d8" />
-        <Bar dataKey="sell" fill="#ffbf00" />
-        <Bar dataKey="revenue" fill="#82ca9d" />
-      </BarChart>
-      <div className="mx-auto">
-        <LineChart
-          width={530}
-          height={250}
-          data={chartData}
-          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="month" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line type="monotone" dataKey="investment" stroke="#ff4000" />
-          <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
-          <Line type="monotone" dataKey="revenue" stroke="	#0000ff" />
-        </LineChart>
+    <div>
+      <h1 className="text-4xl mt-6 font-serif font-semibold">
+        Our Selling and Revenue
+      </h1>
+      <div className="md:flex mt-7">
+        <div className="mx-auto">
+          <BarChart
+            className="mx-auto"
+            width={530}
+            height={250}
+            data={chartData}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="investment" fill="#8884d8" />
+            <Bar dataKey="sell" fill="#ffbf00" />
+            <Bar dataKey="revenue" fill="#82ca9d" />
+          </BarChart>
+        </div>
+        <div className="mx-auto">
+          <LineChart
+            width={530}
+            height={250}
+            data={chartData}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="month" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="investment" stroke="#ff4000" />
+            <Line type="monotone" dataKey="sell" stroke="#82ca9d" />
+            <Line type="monotone" dataKey="revenue" stroke="	#0000ff" />
+          </LineChart>
+        </div>
       </div>
     </div>
   );
