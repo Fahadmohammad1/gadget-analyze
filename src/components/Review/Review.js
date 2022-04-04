037c518725ed8d436d9373fcbd91782ad6146c90
Fahadmohammad1 <205fahadmohammad@gmail.com>
2022-04-04 16:57:37 +0600

    reveiw showed as card

---
+++ src/components/Review/Review.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Review = ({ review }) => {
+  return (
+    <div>
+      <div key={review.id}>
+        <div className=" mx-auto max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <div className="flex flex-col items-center pb-10 mt-5">
+            <img
+              className="mb-3 w-24 h-24 rounded-full shadow-lg"
+              src={review.picture.large}
+              alt="Bonnie"
+            />
+            <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+              {review.name.first}
+            </h5>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              4.5 stars
+            </span>
+            <div>
+              <p>this is description</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Review;
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Review from "../Review/Review";
 
 const Reviews = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    fetch("Reviews.json")
+      .then((res) => res.json())
+      .then((data) => setReviews(data.results));
+  }, []);
   return (
-    <div>
-      <h1>this is review</h1>
+    <div className="md:grid grid-cols-3 mt-5 gap-y-5">
+      {reviews.map((review) => (
+        <Review review={review} key={review.id}></Review>
+      ))}
     </div>
   );
 };
